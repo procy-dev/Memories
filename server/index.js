3d3d7aade383ca5a1381605b31890f0c62ef0fb7
procy-dev <procy.dev@gmail.com>
2021-02-15 16:33:02 -0800

    Added heroku pipeline

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes); //all post routes from post file will start with /posts
 
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
