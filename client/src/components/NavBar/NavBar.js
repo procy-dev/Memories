ea51c92dc9d5bace6603409bb936576169359665
procy-dev <procy.dev@gmail.com>
2021-02-18 13:40:48 -0800

    Added auth form for login, added OAuth via google and logout functionality and started working on classic login form

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint no-unused-vars: 0 */
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 
 import useStyles from './styles';
@@ -7,8 +9,24 @@ import memories from '../../images/memories.png';
 
 const NavBar = () => {
   const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
 
-  const user = null;
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+
+    history.push('/');
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -21,10 +39,10 @@ const NavBar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-            <Button className={classes.logout} variant="contained" color="secondary" onClick={() => {}}>Logout</Button>
+            <Button className={classes.logout} variant="contained" color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="rrimary">Sign In</Button>
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
         )}
       </Toolbar>
     </AppBar>
